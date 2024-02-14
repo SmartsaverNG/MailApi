c6306cd779f2e9e2c2d0d6c0e40cd147b9dd119e
David Horjet <davidojetola956@gmail.com>
2024-02-14 01:32:40 +0100

    code-update

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -50,4 +50,180 @@ const home = async (req, res) => {
   }
 };
 
+exports.applicationForm = async (req, res) => {
+  try {
+    console.log(req.body);
+
+    const {
+      recipientEmail,
+      academic_preference_programme_type,
+      academic_preference_study_destination,
+      academic_preference_prefferred_institute_first,
+      academic_preference_prefferred_institute_second,
+      academic_preference_study_course_first,
+      academic_preference_study_course_second,
+      academic_preference_start_month,
+      student_details_fullname,
+      student_details_dob,
+      student_details_gender,
+      student_details_nationality,
+      student_details_address,
+      student_details_city,
+      student_details_phone,
+      student_details_email,
+      education_history_high_school_start,
+      education_history_high_school_end,
+      education_history_high_school_name,
+      education_history_high_school_certificate,
+      education_history_tertiary_institution,
+      education_history_course_studied,
+      education_history_start_date,
+      education_history_end_date,
+      education_history_grade,
+      guardian_details_fullname,
+      guardian_details_gender,
+      guardian_details_nationality,
+      guardian_details_address,
+      guardian_details_city,
+      guardian_details_phoneNumber,
+      guardian_details_email,
+      guardian_details_guardian_age,
+      emergency_contact_fullname,
+      emergency_contact_gender,
+      emergency_contact_nationality,
+      emergency_contact_address,
+      emergency_contact_city,
+      emergency_contact_phone,
+      emergency_contact_email,
+      crimainal_record_dismissed,
+      crimainal_record_convicted,
+      crimainal_record_denied_visa,
+      financial_info_annual_fund,
+      financial_info_responsibility,
+      standard_testing_date,
+      standard_testing_test_type,
+      standard_testing_score,
+    } = req.body;
+
+    if (
+      !recipientEmail ||
+      !academic_preference_study_destination ||
+      !academic_preference_programme_type ||
+      !academic_preference_prefferred_institute_first ||
+      !academic_preference_prefferred_institute_second ||
+      !academic_preference_study_course_first ||
+      !academic_preference_study_course_second ||
+      !academic_preference_start_month ||
+      !student_details_fullname ||
+      !student_details_dob ||
+      !student_details_gender ||
+      !student_details_nationality ||
+      !student_details_address ||
+      !student_details_city ||
+      !student_details_phone ||
+      !student_details_email ||
+      !education_history_high_school_start ||
+      !education_history_high_school_end ||
+      !education_history_high_school_name ||
+      !education_history_high_school_certificate ||
+      !education_history_tertiary_institution ||
+      !education_history_course_studied ||
+      !education_history_start_date ||
+      !education_history_end_date ||
+      !education_history_grade ||
+      !guardian_details_fullname ||
+      !guardian_details_gender ||
+      !guardian_details_nationality ||
+      !guardian_details_address ||
+      !guardian_details_city ||
+      !guardian_details_phoneNumber ||
+      !guardian_details_email ||
+      !guardian_details_guardian_age ||
+      !emergency_contact_fullname ||
+      !emergency_contact_gender ||
+      !emergency_contact_nationality ||
+      !emergency_contact_address ||
+      !emergency_contact_city ||
+      !emergency_contact_phone ||
+      !emergency_contact_email ||
+      !crimainal_record_dismissed ||
+      !crimainal_record_convicted ||
+      !crimainal_record_denied_visa ||
+      !financial_info_annual_fund ||
+      !financial_info_responsibility ||
+      !standard_testing_date ||
+      !standard_testing_test_type ||
+      !standard_testing_score
+    ) {
+      res.status(400).json({
+        status: false,
+        message: "Please fill all details",
+      });
+    }
+
+    const templateData = {
+      academic_preference_programme_type,
+      academic_preference_study_destination,
+      academic_preference_prefferred_institute_first,
+      academic_preference_prefferred_institute_second,
+      academic_preference_study_course_first,
+      academic_preference_study_course_second,
+      academic_preference_start_month,
+      student_details_fullname,
+      student_details_dob,
+      student_details_gender,
+      student_details_nationality,
+      student_details_address,
+      student_details_city,
+      student_details_phone,
+      student_details_email,
+      education_history_high_school_start,
+      education_history_high_school_end,
+      education_history_high_school_name,
+      education_history_high_school_certificate,
+      education_history_tertiary_institution,
+      education_history_course_studied,
+      education_history_start_date,
+      education_history_end_date,
+      education_history_grade,
+      guardian_details_fullname,
+      guardian_details_gender,
+      guardian_details_nationality,
+      guardian_details_address,
+      guardian_details_city,
+      guardian_details_phoneNumber,
+      guardian_details_email,
+      guardian_details_guardian_age,
+      emergency_contact_fullname,
+      emergency_contact_gender,
+      emergency_contact_nationality,
+      emergency_contact_address,
+      emergency_contact_city,
+      emergency_contact_phone,
+      emergency_contact_email,
+      crimainal_record_dismissed,
+      crimainal_record_convicted,
+      crimainal_record_denied_visa,
+      financial_info_annual_fund,
+      financial_info_responsibility,
+      standard_testing_date,
+      standard_testing_test_type,
+      standard_testing_score,
+    };
+
+    await sendApplicationEmail(recipientEmail, templateData);
+
+    return res.status(200).json({
+      status: true,
+      message: "Application submitted successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      status: false,
+      message: "Internal server error!!",
+    });
+  }
+};
+
 module.exports = { home };
--- services/email.js
+++ services/email.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 const ejs = require('ejs');
 const path = require('path');
 
-const transporter = nodemailer.createTransport({
+const smart_wave_transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     service: "gmail",
     port: 587,
@@ -10,6 +10,20 @@ const transporter = nodemailer.createTransport({
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSCODE,
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+});
+
+const cicon_transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    service: "gmail",
+    port: 587,
+    secure: true,
+    auth: {
+        user: process.env.CICON_EMAIL_USER,
+        pass: process.env.CICON_EMAIL_PASSCODE,
     },
     tls: {
         rejectUnauthorized: false
@@ -29,7 +43,28 @@ exports.sendEmail = async (recipientEmail, templateData) => {
             html: emailTemplate
         };
 
-        await transporter.sendMail(mailOptions);
+        await smart_wave_transporter.sendMail(mailOptions);
+        // console.log(`Verification email sent to ${recipientEmail}`);
+    } catch (error) {
+        console.error('Error sending email:', error);
+        throw new Error('An error occurred while sending the email');
+    }
+};
+
+exports.sendApplicationEmail = async (recipientEmail, templateData) => {
+    try {
+        const templatePath = path.join(__dirname, '../views/application_template.ejs');
+
+        const emailTemplate = await ejs.renderFile(templatePath, templateData);
+
+        const mailOptions = {
+            from: `"no reply" ${process.env.EMAIL_USER}`,
+            to: recipientEmail,
+            subject: 'Incoming Message! 👋',
+            html: emailTemplate
+        };
+
+        await cicon_transporter.sendMail(mailOptions);
         // console.log(`Verification email sent to ${recipientEmail}`);
     } catch (error) {
         console.error('Error sending email:', error);
